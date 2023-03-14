59d1e4ac099d0ea1bc07e23fd01fcf08cbda3b14
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2023-03-13 22:04:14 -0300

    Arrow function

--- funcao/arrowFunction1.js
+++ funcao/arrowFunction1.js
@@ -1,18 +1,18 @@
 let dobro = function (a) {
     return 2 * a
 }
-
+// Reescrevendo com arrow function
 dobro = (a) => {
     return 2 * a 
 }
-
+// Mais reduzida
 dobro = a => 2 * a // return implícito
 console.log(dobro(Math.PI))
 
 let ola = function () {
     return 'Olá'
 }
-
+// Reescrevendo com arrow function
 ola = () => 'Olá'
 ola = _ => 'Olá' // possui um param
 console.log(ola())
--- funcao/thisEBind1.js
+++ funcao/thisEBind1.js
@@ -9,5 +9,6 @@ pessoa.falar()
 const falar = pessoa.falar
 falar() // conflito entre paradigmas: funcional e OO
 
+// O bind vai referenciar o this, no caso pessoa
 const falarDePessoa = pessoa.falar.bind(pessoa)
 falarDePessoa()
